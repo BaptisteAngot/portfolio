e8cc8b643ba4f4fff11c89ce2da2a1025b98dd2e
baptiste <bat76640@gmail.com>
2025-03-05 23:53:30 +0100

    Correction du plugin de copie pour .htaccess

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          { src: 'public/.htaccess', dest: '' } // Copie .htaccess dans dist/
+        ]
+      })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
